e19cd658881c79f7fe75a508780c8792316d92c2
Thomas Ken Ronaldi <thomken21@outlook.com>
2023-07-29 00:01:59 +0700

    Adding first push to Git

---
+++ routers/cleansing.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from fastapi import Query
+from services.cleansing import cleansing
+
+router = APIRouter()
+
+@router.get("/cleansing")
+async def text_cleansing(sentence: str = Query(default="")):
+    
+    result= await cleansing(sentence)
+    return result
---
+++ services/cleansing.py
@@ -0,0 +1,49 @@
+from services import AppService
+import re
+from fastapi import status
+import pandas as pd
+ 
+async def cleanse_text(text):
+    # Menghapus karakter non-alfanumerik
+    text = re.sub(r'\W+', ' ', text)
+ 
+    # Menghapus karakter berulang
+    text = re.sub(r'(.)\1+', r'\1\1', text)
+ 
+    # Mengubah semua huruf menjadi huruf kecil
+    text = text.lower()
+ 
+    # Menghapus spasi di awal dan akhir teks
+    text = text.strip()
+ 
+    return text
+
+async def clean_file(file):
+    df =  pd.read_csv(file)
+ 
+async def cleansing(sentence):
+        try:
+            data = await cleanse_text(text=sentence)
+            print(data)
+            content = {
+                "ok": True,
+                "code": status.HTTP_200_OK,
+                "data": data,
+                "message": "Success",
+            }
+            return content
+        except Exception as e:
+            return e
+async def file_cleansing(filename):
+        try:
+            data = await cleanse_text(text=filename)
+            print(data)
+            content = {
+                "ok": True,
+                "code": status.HTTP_200_OK,
+                "data": data,
+                "message": "Success",
+            }
+            return content
+        except Exception as e:
+            return e
